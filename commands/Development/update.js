33c1d6f9959fe99f2efea0437f865d5a9fc14649
Elion Wang <74402660+elionwang@users.noreply.github.com>
2021-09-29 13:56:39 +0000

    commit

--- commands/Development/update.js
+++ commands/Development/update.js
@@ -4,10 +4,10 @@ module.exports = {
   usage: '_update',
   category: 'Development',
   code: `$updateCommands
-  $author[$userTag[$clientID];https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif]
+  $author[$userTag[$clientID];https://raw.githubusercontent.com/elionwang/TBWiBotRework/master/0c675a8e1061478d2b7b21b330093444.gif]
   $title[Commands Update]
   $description[Alle commands ($commandsCount) zijn geüpdatet $customEmoji[bs_thanks;861677703455703061].
   $color[FFD700]
   $addCmdReactions[$customEmoji[bs_$randomText[smile;bothered;laugh;sad;shocked;stunned;frenzied;angry;rage;annoyed;ecstatic;happy;speechless];861677703455703061]]
-  $onlyForIDs[428272829553967106;{author:$userTag[$clientID]:https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif}{color:RED}{title:Error $customEmoji[bs_stunned;861677703455703061]}{description:Je hebt geen permission voor deze command $customEmoji[bs_bothered;861677703455703061].}]`
+  $onlyForIDs[428272829553967106;{author:$userTag[$clientID]:https://raw.githubusercontent.com/elionwang/TBWiBotRework/master/0c675a8e1061478d2b7b21b330093444.gif}{color:RED}{title:Error $customEmoji[bs_stunned;861677703455703061]}{description:Je hebt geen permission voor deze command $customEmoji[bs_bothered;861677703455703061].}]`
 }
